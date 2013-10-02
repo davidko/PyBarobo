33f4dbd402cf3ba03aad31187b4631a04d29818f
David Ko <david.dk.ko@gmail.com>
2013-10-02 11:28:35 -0700

    Updated info in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from distutils.core import setup
 
 setup(name='PyBarobo',
     version='0.1.0',
-    description='Native Pythor Barobo robotics control library',
+    description='Native Python Barobo robotics control library',
     author='David Ko',
     author_email='david@barobo.com',
+    maintainer='David Ko',
+    maintainer_email='david@barobo.com',
+    url='http://www.barobo.com',
+    license='GPL',
+    platforms='any',
     packages=['barobo']
     #py_modules=['pybarobo']
     )
