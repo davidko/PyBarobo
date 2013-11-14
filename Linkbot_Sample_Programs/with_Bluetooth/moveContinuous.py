00a26479824009243bb5b654423fb5eb8f0fdbda
David Ko <david.dk.ko@gmail.com>
2013-11-14 15:12:32 -0800

    Added testLinkPod demotest

--- Linkbot_Sample_Programs/with_Bluetooth/moveContinuous.py
+++ Linkbot_Sample_Programs/with_Bluetooth/moveContinuous.py
@@ -3,16 +3,21 @@
 import barobo
 from barobo import Linkbot, BaroboCtx
 import time
+import sys
 
 if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print('Usage: {0} <Bluetooth MAC Address>'.format(sys.argv[0]))
+    sys.exit(0)
   linkbot = Linkbot()
-#linkbot.connectBluetooth('00:06:66:45:D9:F5')
-  linkbot.connectBluetooth('00:06:66:4D:F6:6F')
+  linkbot.connectMobotBluetooth(sys.argv[1])
 
   linkbot.resetToZero()
-  print ("Moving joints forwards for 4 seconds...")
+  print ("Moving joints forwards...")
   linkbot.setJointSpeeds(-120, 120, 120)
   linkbot.moveContinuous(barobo.ROBOT_FORWARD, barobo.ROBOT_FORWARD, barobo.ROBOT_FORWARD)
+  raw_input("Press enter to stop.")
+  linkbot.moveContinuous(barobo.ROBOT_BACKWARD, barobo.ROBOT_BACKWARD, barobo.ROBOT_BACKWARD)
   raw_input("Press enter to stop.")
   linkbot.resetToZero()
   linkbot.stop()
