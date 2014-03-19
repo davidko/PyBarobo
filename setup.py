ab84ef8b87094c8c9ba6ec6afe37b9365187022b
David Ko <david.dk.ko@gmail.com>
2014-03-19 12:43:00 -0700

    Updated version to 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PyBarobo',
-    version='0.1.6',
+    version='0.1.7',
     description='Native Python Barobo robotics control library',
     author='David Ko',
     author_email='david@barobo.com',
