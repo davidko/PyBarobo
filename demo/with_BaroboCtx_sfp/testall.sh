7cb0da30cad06c6be67bf6e09b8c34fb96a6f447
David Ko <david.dk.ko@gmail.com>
2014-04-24 14:21:14 -0700

    Moved/added demo files

---
+++ demo/with_BaroboCtx_sfp/testall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -lt 1 ] ; then
+  echo "Usage: $0 <com_port> [serialID]"
+  exit
+fi
+
+for i in *.py; do
+  echo "Running $i ..."
+  ./$i $1 $2
+  echo "Press Enter to continue, or type q to quit"
+  read text
+  if [ "$text" == "q" ] ; then
+    exit
+  fi
+  sleep 1
+done
